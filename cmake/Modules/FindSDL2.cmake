7ab622d6729ad9aa3fe5c182b887f1f66b560935
Dugan Chen <thedoogster@gmail.com>
2014-06-21 18:59:48 -0700

    Comment added about building on Windows

--- cmake/Modules/FindSDL2.cmake
+++ cmake/Modules/FindSDL2.cmake
@@ -1,2 +1,5 @@
+# This works on systems that use pkgconfig. If you want to use it on
+# systems that don't, such as Windows, you'll have to add to this.
+
 INCLUDE(FindPkgConfig)
 PKG_SEARCH_MODULE(SDL2 REQUIRED sdl2)
