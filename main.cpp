22b99f7603f4543e34219cfb04a47801a7cc0a8e
Dugan Chen <thedoogster@gmail.com>
2014-06-21 17:29:01 -0700

    InitGL is eliminated

--- main.cpp
+++ main.cpp
@@ -8,10 +8,16 @@
 int main(int argc, char *argv[])
 {
     GameApp theGame;
-	
-	theGame.InitApp();
+
+    auto contextFlags = SDL_WINDOW_SHOWN|SDL_WINDOW_OPENGL;
+
+    // Create a 640 by 480 window.
+    theGame.InitializeSDL(640, 480, contextFlags);
+    theGame.CreateOrthographicProjection(4.0, 3.0);
+    theGame.InstallTimer();
+
 	theGame.EventLoop();
     theGame.Cleanup();
-    
+
     return 0;
 }
